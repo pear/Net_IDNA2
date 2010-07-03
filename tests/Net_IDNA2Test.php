a377c8a6051dae16c6c38da14f8b730b459a2fbc
Daniel O'Connor <clockwerx@php.net>
2010-07-03 12:57:19 +0000

    Updated test case
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_IDNA2/trunk@300971 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Net_IDNA2Test.php
+++ tests/Net_IDNA2Test.php
@@ -11,6 +11,6 @@ class Net_IDNA2Test extends PHPUnit_Framework_TestCase {
     public function testShouldDecodePortNumbersFragmentsAndUrisCorrectly() {
         $result = $this->idn->decode('http://www.xn--ml-6kctd8d6a.org:8080/test.php?arg=1#fragment');
 
-        $this->assertSame("http://www.╨╡╤à╨░m╤Çl╨╡.org:8080/test.php?arg=1#fragment", $result);
+        $this->assertSame("http://www.\xD0\xB5\xD1\x85\xD0\xB0m\xD1\x80l\xD0\xB5.org:8080/test.php?arg=1#fragment", $result);
     }
 }
