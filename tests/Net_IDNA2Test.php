68c072ef19eb673989473644ed34102f52fdb00b
Stefan Neufeind <neufeind@php.net>
2010-12-09 15:44:43 +0000

    #18106: fixed package.xml (thanks to glen)
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_IDNA2/trunk@306129 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Net_IDNA2Test.php
+++ tests/Net_IDNA2Test.php
@@ -2,21 +2,39 @@
 require_once 'Net/IDNA2.php';
 require_once 'PHPUnit2/Framework/TestCase.php';
 
-class Net_IDNA2Test extends PHPUnit2_Framework_TestCase {
-
-    public function setUp() {
+class Net_IDNA2Test extends PHPUnit2_Framework_TestCase
+{
+    /**
+     * Initialise tests
+     *
+     * @return void
+     */
+    public function setUp()
+    {
         $this->idn = new Net_IDNA2();
     }
 
-    public function testShouldDecodePortNumbersFragmentsAndUrisCorrectly() {
-// not sure where this testcase came from, but it's wrong
-//        $result = $this->idn->decode('http://www.xn--ml-6kctd8d6a.org:8080/test.php?arg1=1&arg2=2#fragment');
-//        $this->assertSame("http://www.╨╡╤à╨░m╤Çl╨╡.org:8080/test.php?arg=1#fragment", $result);
+    /**
+     * Test if a complete URL consisting also of port-number etc. will be decoded just fine
+     *
+     * @return void
+     */
+    public function testShouldDecodePortNumbersFragmentsAndUrisCorrectly()
+    {
+        // not sure where this testcase came from, but it's wrong
+        // $result = $this->idn->decode('http://www.xn--ml-6kctd8d6a.org:8080/test.php?arg1=1&arg2=2#fragment');
+        // $this->assertSame("http://www.╨╡╤à╨░m╤Çl╨╡.org:8080/test.php?arg=1#fragment", $result);
         $result = $this->idn->decode('http://xn--tst-qla.example.com:8080/test.php?arg1=1&arg2=2#fragment');
         $this->assertSame("http://täst.example.com:8080/test.php?arg1=1&arg2=2#fragment", $result);
     }
 
-    public function testEncodingForGermanEszettUsingIDNA2003() {
+    /**
+     * Test encoding of German letter Eszett according to the original standard (IDNA2003)
+     *
+     * @return void
+     */
+    public function testEncodingForGermanEszettUsingIDNA2003()
+    {
         // make sure to use 2003-encoding
         $this->idn->setParams('version', '2003');
         $result = $this->idn->encode('http://www.straße.example.com/');
@@ -24,7 +42,13 @@ class Net_IDNA2Test extends PHPUnit2_Framework_TestCase {
         $this->assertSame("http://www.strasse.example.com/", $result);
     }
 
-    public function testEncodingForGermanEszettUsingIDNA2008() {
+    /**
+     * Test encoding of German letter Eszett according to the "new" standard (IDNA2005/IDNAbis)
+     *
+     * @return void
+     */
+    public function testEncodingForGermanEszettUsingIDNA2008()
+    {
         // make sure to use 2008-encoding
         $this->idn->setParams('version', '2008');
         $result = $this->idn->encode('http://www.straße.example.com/');
